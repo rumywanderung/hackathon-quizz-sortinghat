f4dd6bb80a4542e23b44e6d09b4cfe213ebd6e41
Sinsi <sylvienumuhire@gmail.com>
2019-11-27 15:37:07 +0100

    ajouter infos premiere questiv dans la div prévue à cet effet

--- src/index.js
+++ src/index.js
@@ -5,22 +5,35 @@ fetch('../public/assets/data/questions.json')
   .then((data) => {
     // target le body
     const monBody = $('body');
-    // créer div qui accueillera les questions
-    // const maQuestion = $('<div id="maQuestion"></div>');
-    const maQuestion = $(`
-    <div id="maQuestion">
-      <p></p>
-      <div>
-        ${data.questions.reponse}
-      </div>
-    </div>`);
-    console.log(data.questions);
-    monBody.append(maQuestion);
 
-    // pour chaque question créer une div
+    // pour chaque question créer une div coloré par le css
     data.questions.forEach((question) => {
       monBody.append(`<section id="question${question.id}"></section>`);
     });
+
+    // créer div qui accueillera les questions
+    const maQuestion = $('<div id="maQuestion"></div>');
+    maQuestion.append(`
+      <p id="${data.questions[0].id}">${data.questions[0].question}</p>
+      <div>
+      </div>
+    `);
+
+    data.questions[0].reponses.forEach((reponse) => {
+      // console.log(reponse.rep);
+      $(maQuestion.children()[1]).append(`
+      <p class="response">${reponse.rep}</p>
+      `);
+    });
+
+    // ajouter la question au body
+    monBody.append(maQuestion);
+
+    $('.response').click(function () {
+      console.log($(this).parent().parent().children()[0]);
+      // $(this.parent().parent().children()[0]).
+    });
+
 
     // append la premiere question
     // maQuestion.append(`<p>${data.questions[0].question}</p>`);
